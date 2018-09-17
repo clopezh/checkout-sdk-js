vmparra/checkout-sdk-js-old

fix(payment): INT-751 Show Masterpass button in payments section into square form

src/payment/strategies/square/square-form.ts
@@ -19,6 +19,39 @@ export interface SquareFormOptions {
     cvv: SquareFormElement;
     expirationDate: SquareFormElement;
     postalCode: SquareFormElement;
+    masterpass: SquareFormElement;
+}
+
+export interface Error {
+    type: string;
+    message: string;
+    field: string;
+}
+
+export interface CardData {
+    cardBrand: CardBrand;
+    last4: number;
+    expMonth: number;
+    expYear: number;
+    billingPostalCode: string;
+    digitalWalletType: DigitalWalletType;
+}
+
+export enum CardBrand {
+    americanExpress,
+    discover,
+    discoverDiners,
+    JCB,
+    masterCard,
+    unionPay,
+    unknown,
+    visa
+}
+
+export enum DigitalWalletType {
+    applePay,
+    masterpass,
+    none,
 }
 
 /**

clopezh/checkout-sdk-js

src/payment/strategies/square/square-form.ts
@@ -19,6 +19,68 @@ export interface SquareFormOptions {
     cvv: SquareFormElement;
     expirationDate: SquareFormElement;
     postalCode: SquareFormElement;
+    masterpass: SquareFormElement;
+}
+export interface LineItem {
+    label: string;
+    amount: string;
+    pending: boolean;
+}
+
+export interface SquarePaymentRequest {
+    requestShippingAddress: boolean;
+    requestBillingInfo: boolean;
+    shippingContact?: Contact;
+    countryCode: string;
+    currencyCode: string;
+    lineItems?: LineItem[];
+    total: LineItem;
+}
+
+export interface NonceGenerationError {
+    type: string;
+    message: string;
+    field: string;
+}
+
+export interface CardData {
+    card_brand: CardBrand;
+    last_4: number;
+    exp_month: number;
+    exp_year: number;
+    billing_postal_code: string;
+    digital_wallet_type: DigitalWalletType;
+}
+
+export interface Contact {
+    familyName: string;
+    givenName: string;
+    email: string;
+    country: string;
+    countryName: string;
+    region: string;
+    city: string;
+    addressLines: string[];
+    postalCode: string;
+    phone: string;
+}
+
+export enum CardBrand {
+    americanExpress = 'AMERICAN_EXPRESS',
+    discover = 'DISCOVER',
+    discoverDiners = 'DISCOVER_DINERS',
+    JCB = 'JCB',
+    masterCard = 'MASTERCARD',
+    unionPay = 'CHINA_UNIONPAY',
+    unknown = 'OTHER_BRAND',
+    visa = 'VISA',
+    squareGift = 'SQUARE_GIFT_CARD',
+}
+
+export enum DigitalWalletType {
+    applePay = 'APPLEPAY',
+    masterpass = 'MASTERPASS',
+    none = 'NONE',
 }
 
 /**
@@ -39,7 +101,14 @@ export interface SquareFormElement {
 export interface SquareFormCallbacks {
     paymentFormLoaded?(form: SquarePaymentForm): void;
     unsupportedBrowserDetected?(): void;
-    cardNonceResponseReceived?(errors: any, nonce: string): void;
+    cardNonceResponseReceived?(
+        errors?: NonceGenerationError[],
+        nonce?: string,
+        cardData?: CardData,
+        billingContact?: Contact,
+        shippingContact?: Contact): void;
+    methodsSupported?(methods: { [key: string]: boolean }): void;
+    createPaymentRequest?(): void;
 }
 
 export type SquareFormFactory = (options: SquareFormOptions) => SquarePaymentForm;