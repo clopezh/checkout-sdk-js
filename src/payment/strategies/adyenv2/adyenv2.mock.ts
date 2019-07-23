0390f9ac89f0635997d8333a8917cc6a05c8d2a7
Victor Parra <victor.parra@bigcommerce.com>
2019-07-23 08:41:08 -0500

    feat(payment): INT-1783 Support Adyen v2 payment strategy(REBASED)

---
+++ src/payment/strategies/adyenv2/adyenv2.mock.ts
@@ -0,0 +1,76 @@
+import { OrderRequestBody } from '../../../order';
+import { PaymentInitializeOptions } from '../../payment-request-options';
+
+import {
+    AdyenCardState,
+    AdyenCheckout,
+    AdyenConfiguration
+} from './adyenv2';
+
+export function getAdyenCheckout(): AdyenCheckout {
+    return {
+        create: jest.fn(() => {
+            return {
+                mount: jest.fn(),
+                unmount: jest.fn(),
+            };
+        }),
+    };
+}
+
+export function getAdyenConfiguration(): AdyenConfiguration {
+    return {
+        environment: 'test',
+        originKey: 'YOUR_ORIGIN_KEY',
+    };
+}
+
+export function getAdyenInitializeOptions(): PaymentInitializeOptions {
+    return {
+        methodId: 'adyenv2',
+        adyenv2: {
+            containerId: 'adyen-component-field',
+            options: {
+                hasHolderName: true,
+                styles: {},
+                placeholders: {},
+            },
+        },
+    };
+}
+
+export function getAdyenOrderRequestBody(): OrderRequestBody {
+    return {
+        payment: {
+            methodId: 'adyenv2',
+        },
+    };
+}
+
+function getCardState() {
+    return {
+        data: {
+            paymentMethod: {
+                encryptedCardNumber: 'CARD_NUMBER',
+                encryptedExpiryMonth: 'EXPIRY_MONTH',
+                encryptedExpiryYear: 'EXPIRY_YEAR',
+                encryptedSecurityCode: 'CVV',
+                type: 'scheme',
+            },
+        },
+    };
+}
+
+export function getValidCardState(): AdyenCardState {
+    return {
+        ...getCardState(),
+        isValid: true,
+    };
+}
+
+export function getInvalidCardState(): AdyenCardState {
+    return {
+        ...getCardState(),
+        isValid: false,
+    };
+}
