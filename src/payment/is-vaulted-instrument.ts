f61d5a941cb7a56aa2fcac95e0cb1e39c3f9ca21
Carlos Lopez <clopez@intersysconsulting.com>
2019-11-25 15:51:46 -0600

    feat(payment): INT-1990 Use browserInfo from browser

--- src/payment/is-vaulted-instrument.ts
+++ src/payment/is-vaulted-instrument.ts
@@ -1,5 +1,11 @@
-import { PaymentInstrument, VaultedInstrument } from './payment';
+import { AdyenV2Instrument, FormattedPayload, PaymentInstrument, VaultedInstrument} from './payment';
 
 export default function isVaultedInstrument(instrument: PaymentInstrument): instrument is VaultedInstrument {
+    const formattedPayload = (instrument as FormattedPayload<AdyenV2Instrument>).formattedPayload;
+
+    if (formattedPayload && formattedPayload.bigpay_token) {
+        return Boolean(formattedPayload.bigpay_token.token);
+    }
+
     return Boolean((instrument as VaultedInstrument).instrumentId);
 }
